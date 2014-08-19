2366545aeab2c246beeecf2dc5c5f3b7a6ee84e3
azu <azuciao@gmail.com>
2014-08-19 17:56:23 +0900

    use AZEncodeURIComponent

--- lib/URLQueryBuilder.m
+++ lib/URLQueryBuilder.m
@@ -4,28 +4,19 @@
 
 
 #import "URLQueryBuilder.h"
-
+#import "AZEncodeURIComponent.h"
 
 @implementation URLQueryBuilder {
-
-}
-NSString *encodeURIComponent(NSString *string) {
-    return (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(
-        NULL,
-        (__bridge CFStringRef)string,
-        NULL,
-        CFSTR("!*'();:@&=+$,/?%#[]"),
-        CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
 
 }
 
 + (NSString *)buildQueryWithDictionary:(NSDictionary *) dictionary {
     NSMutableArray *query = [NSMutableArray array];
     for (NSString *key in [dictionary allKeys]) {
-        id value = [dictionary objectForKey:key];
+        id value = dictionary[key];
         [query addObject:[NSString stringWithFormat:@"%@=%@",
-                                                    encodeURIComponent([key description]),
-                                                    encodeURIComponent([value description])]];
+                                                    AZEncodeURIComponent([key description]),
+                                                    AZEncodeURIComponent([value description])]];
     }
     return [query componentsJoinedByString:@"&"];
 }
